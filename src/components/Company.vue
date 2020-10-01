3f9579ee29f5e3b207d401832f8e6455a33b4529
Priscila Souza <52748965+priscilasouza@users.noreply.github.com>
2020-10-01 19:48:29 -0300

    Create Company.vue (#3)

---
+++ src/components/Company.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="company">
+    <ul>
+      <li>Colegio Ari de Sá</li>
+      <li>SAS Plataforma de Educação</li>
+      <li>Arco Educação</li>
+      <li>Youse</li>
+      <li>PicPay</li>
+    </ul>
+  </div>
+</template>
